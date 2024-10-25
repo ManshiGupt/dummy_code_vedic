35b1992c5ec9e46203e0bf37a58ccb36bcb9d0ff
MANSHI GUPTA <133130525+ManshiGupt@users.noreply.github.com>
2024-10-25 22:26:59 +0530

    Create javascriptObjectLoop.js

---
+++ javascriptObjectLoop.js
@@ -0,0 +1,21 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+ a =[
+    "aditya",
+    "jkl"
+]
+b={
+    man: "manshi",
+    nam: "jkl",
+    kaam: "io"
+}
+
+const o = ["we", "are", "love"];
+
+console.log(a.includes(o))
+// Object.keys(a).forEach((ram)=>{
+//     // 
+//     a[ram]= b[ram]
+//     console.log('*'+ ram );
+//     console.log(a[ram]);
+// })
